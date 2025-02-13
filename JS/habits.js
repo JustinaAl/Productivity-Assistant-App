1958862c73e83f528d537a34c28022354631d86f
JustinaAl <juste.paskeviciute@gmail.com>
2025-02-13 12:42:36 +0100

    pushes to db

--- JS/habits.js
+++ JS/habits.js
@@ -21,16 +21,13 @@ let createHabit = () => {
         document.body.querySelector("#habitsWrapper").append(wrapper);
         
         document.querySelector("#moreInfo").append(mainDiv);
-        document.querySelector("#saveHabit").addEventListener("click", addHabit);
+        document.querySelector("#saveHabit").addEventListener("click", pushHabitdb);
     }
 };
 
 document.querySelector("#createNew").addEventListener("click", createHabit);
 
 let addHabit = () => {
-    let value = 0;
-    let title = document.querySelector("#habitTitle").value;
-    let chosenPriority = document.querySelector("#prioritySelection").value;
     let habitBox = document.createElement("div");
     habitBox.classList.add("habitBox");
     habitBox.innerHTML = `
@@ -69,3 +66,66 @@ let addHabit = () => {
     plus.addEventListener("click", increaseValue);
     minus.addEventListener("click", decreaseValue);
 };
+
+let pushHabitdb = async () => {
+    let userId = sessionStorage.getItem('userId');
+    let value = 0;
+    let title = document.querySelector("#habitTitle").value;
+    let chosenPriority = document.querySelector("#prioritySelection").value;
+    
+        await axios.post("http://localhost:5000/habits", {
+            userId,
+            title,
+            reps:value,
+            priority:chosenPriority
+        });
+};
+
+let loadPage = async()=>{
+    let userId = sessionStorage.getItem('userId');
+    let habits = await axios.get("http://localhost:5000/habits")
+    let filteredHabits = habits.data.filter(habit => habit.userId === userId);
+
+    filteredHabits.forEach(element => {
+        let habitBox = document.createElement("div");
+        habitBox.classList.add("habitBox");
+        habitBox.innerHTML = `
+            <div class="informationBox">
+                <p>${element.title}</p>
+                <p>Priority: ${element.priority}</p>
+            </div>
+            <div class="repetitionBox">
+                <p>Repetitions</p>
+                <div class="counter">
+                    <button class="minus">-</button>
+                    <p class="value">${element.reps}</p>
+                    <button class="plus">+</button>
+                </div>
+            </div>`;
+    
+        document.querySelector("#habitsMain").append(habitBox);
+
+        let value = 0;
+        let plus = habitBox.querySelector(".plus");
+        let minus = habitBox.querySelector(".minus");
+        let currentValue = habitBox.querySelector(".value");
+        
+        let increaseValue = () => {
+            value += 1;
+            currentValue.textContent = `${value}`;
+        };
+    
+        let decreaseValue = () => {
+            if (value > 0) {
+                value -= 1;
+                currentValue.textContent = `${value}`;
+            }
+        };
+    
+        plus.addEventListener("click", increaseValue);
+        minus.addEventListener("click", decreaseValue);
+    });
+    
+}
+
+loadPage();
