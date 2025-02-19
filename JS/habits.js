bb35325fadc106ac8b32dcc501462aee3a31ef55
JustinaAl <juste.paskeviciute@gmail.com>
2025-02-19 11:41:39 +0100

    refresh button

--- JS/habits.js
+++ JS/habits.js
@@ -50,12 +50,12 @@ let getHabits = async () => {
 //Create a habit box for posting to the page
 let createHabitBox = (habit) => {
   if (habit.priority == "0") {
-   habit.priority = "low" ;   
-  }else if (habit.priority == "1") {
-    habit.priority = "medium" ;   
-   }else if (habit.priority == "2") {
-    habit.priority = "high" ;   
-   }
+    habit.priority = "low";
+  } else if (habit.priority == "1") {
+    habit.priority = "medium";
+  } else if (habit.priority == "2") {
+    habit.priority = "high";
+  }
   let habitBox = document.createElement("div");
   habitBox.classList.add("habitBox");
   habitBox.innerHTML = `
@@ -65,7 +65,10 @@ let createHabitBox = (habit) => {
         </div>
         <div class="more">
             <div class = "counterWrap">
-                <p>Repetitions</p>
+                <div class="rep">
+                  <p>Repetitions</p>
+                  <button class="refresh"><i class="fa-solid fa-rotate-right" style="color: #ffffff;"></i></button>
+                </div>
                 <div class="counter">
                     <button class="minus"><i class="fa-solid fa-minus" style="color: #ffffff;"></i></button>
                     <p class="value">${habit.reps}</p>
@@ -75,45 +78,50 @@ let createHabitBox = (habit) => {
             <div class="iconBox"><i class="fa-solid fa-ellipsis-vertical"></i></div>
         </div>`;
   return habitBox;
+
 };
 
 //Counts repetitions
-let countRepetitions = async(habitBox, value, habitId) => {
+let countRepetitions = async (habitBox, value, habitId) => {
   let plus = habitBox.querySelector(".plus");
   let minus = habitBox.querySelector(".minus");
+  let refreshBtn = habitBox.querySelector(".refresh");
 
-  let increaseValue = async() => {
+  let increaseValue = async () => {
     let data = await axios.get(`http://localhost:5001/habits/${habitId}`);
-    let valueNumber = (+data.data.reps);
+    let valueNumber = +data.data.reps;
     valueNumber += 1;
 
-    await axios.patch(`http://localhost:5001/habits/${habitId}`,{
-        reps: `${valueNumber}`
-    })
-    
+    await axios.patch(`http://localhost:5001/habits/${habitId}`, {
+      reps: `${valueNumber}`,
+    });
   };
 
-  let decreaseValue = async() => {
+  let decreaseValue = async () => {
     if (value > 0) {
-        let data = await axios.get(`http://localhost:5001/habits/${habitId}`);
-        let valueNumber = (+data.data.reps);
-        valueNumber -=1;
+      let data = await axios.get(`http://localhost:5001/habits/${habitId}`);
+      let valueNumber = +data.data.reps;
+      valueNumber -= 1;
 
-    await axios.patch(`http://localhost:5001/habits/${habitId}`,{
-        reps: `${valueNumber}`
-    })
+      await axios.patch(`http://localhost:5001/habits/${habitId}`, {
+        reps: `${valueNumber}`,
+      });
     }
   };
 
+  let refresh = async() => {
+    await axios.patch(`http://localhost:5001/habits/${habitId}`, {
+      reps: `0`,
+    });
+  }
+
   plus.addEventListener("click", increaseValue);
   minus.addEventListener("click", decreaseValue);
-
-  
+  refreshBtn.addEventListener("click", refresh);
 };
 
-
 //More info
-let openMoreInfo = async(habitBox, element) => {
+let openMoreInfo = async (habitBox, element) => {
   let i = habitBox.querySelector(".iconBox i");
   let iconBox = habitBox.querySelector(".iconBox");
   i.addEventListener("click", () => {
@@ -142,23 +150,22 @@ let openMoreInfo = async(habitBox, element) => {
       editHabit(element.id, habitBox);
       let saveBtn = document.createElement("button");
       saveBtn.classList.add("save");
-      saveBtn.innerHTML= `<i class="fa-solid fa-check" style="color: #ffffff;"></i><p>Save</p>`
+      saveBtn.innerHTML = `<i class="fa-solid fa-check" style="color: #ffffff;"></i><p>Save</p>`;
       iconBox.append(saveBtn);
       editBtn.remove();
 
-      saveBtn.addEventListener("click", async()=>{
+      saveBtn.addEventListener("click", async () => {
         let updatedTitle = habitBox.querySelector("input").value;
         let updatedPriority = habitBox.querySelector("select").value;
         try {
-            await axios.patch(`http://localhost:5001/habits/${element.id}`, {
-                title: updatedTitle,
-                priority: updatedPriority
-              });
+          await axios.patch(`http://localhost:5001/habits/${element.id}`, {
+            title: updatedTitle,
+            priority: updatedPriority,
+          });
         } catch (error) {
-            console.log("",error);
+          console.log("", error);
         }
-        
-      })
+      });
     });
   });
 };
@@ -202,49 +209,47 @@ let editHabit = async (habitId, habitBox) => {
   priority.remove();
 };
 
-
 //Load page
 let loadPage = async () => {
   sort();
   let filteredHabits = await getHabits();
-  
-  if(sessionStorage.getItem("selectedPriorites")){
+
+  if (sessionStorage.getItem("selectedPriorites")) {
     filteredHabits = await filterPage();
   }
 
   let valueInStorage = sessionStorage.getItem("sorting");
 
-  if(valueInStorage){
-    filteredHabits.forEach(item=>{
-      item.reps = Number(item.reps)
-    })
-  
-    let setOrder = async() =>{
-      filteredHabits.forEach(element => {
-        if(element.priority === "low"){
+  if (valueInStorage) {
+    filteredHabits.forEach((item) => {
+      item.reps = Number(item.reps);
+    });
+
+    let setOrder = async () => {
+      filteredHabits.forEach((element) => {
+        if (element.priority === "low") {
           element.priority = 0;
-        }else if(element.priority === "medium"){
+        } else if (element.priority === "medium") {
           element.priority = 1;
-        }else if(element.priority === "high")
-          element.priority = 2;
+        } else if (element.priority === "high") element.priority = 2;
       });
+    };
+
+    if (valueInStorage === "pLowHigh") {
+      setOrder();
+      filteredHabits.sort((a, b) => a.priority - b.priority);
+    } else if (valueInStorage === "pHighLow") {
+      setOrder();
+      filteredHabits.sort((a, b) => b.priority - a.priority);
+    } else if (valueInStorage === "rLowHigh") {
+      filteredHabits.sort((a, b) => a.reps - b.reps);
+    } else if (valueInStorage === "rHighLow") {
+      filteredHabits.sort((a, b) => b.reps - a.reps);
     }
-    
-  if (valueInStorage === "pLowHigh") {
-    setOrder();
-    filteredHabits.sort((a, b) => a.priority - b.priority);
-  } else if (valueInStorage === "pHighLow") {
-    setOrder();
-    filteredHabits.sort((a, b) => b.priority - a.priority);
-  }else if (valueInStorage === "rLowHigh") {
-    filteredHabits.sort((a, b) => a.reps - b.reps);
-  }else if (valueInStorage === "rHighLow") {
-    filteredHabits.sort((a, b) => b.reps - a.reps);
-  }
   }
 
-  document.querySelector("#allHabits").innerHTML = '';
-  filteredHabits.forEach(element => {
+  document.querySelector("#allHabits").innerHTML = "";
+  filteredHabits.forEach((element) => {
     let habitBox = createHabitBox(element);
     document.querySelector("#allHabits").append(habitBox);
 
@@ -255,53 +260,59 @@ let loadPage = async () => {
   });
 };
 
-let filterPage =async()=>{
+let filterPage = async () => {
   let checked = sessionStorage.getItem("selectedPriorites");
   let filteredHabits = await getHabits();
-  if(checked){
-    let checkedArray = checked.split(',');
+  if (checked) {
+    let checkedArray = checked.split(",");
 
-    let allCheckboxes= document.querySelectorAll("input[type='checkbox']");
+    let allCheckboxes = document.querySelectorAll("input[type='checkbox']");
 
-    allCheckboxes.forEach(checkbox=>{
+    allCheckboxes.forEach((checkbox) => {
       if (checkedArray.includes(checkbox.value)) {
         checkbox.checked = true;
       }
-    })
+    });
 
-    if(document.querySelectorAll("input[type='checkbox']:checked").length>0){
-      filteredHabits = filteredHabits.filter(habit => checkedArray.includes(habit.priority));
+    if (
+      document.querySelectorAll("input[type='checkbox']:checked").length > 0
+    ) {
+      filteredHabits = filteredHabits.filter((habit) =>
+        checkedArray.includes(habit.priority)
+      );
     }
   }
   return filteredHabits;
-}
+};
 
 //Pusher filtering details to local storage
-let filter = () =>{
-    let selectedPriorities = [];
-    let high = document.querySelector("#highCheckbox");
-    let medium = document.querySelector("#mediumCheckbox");
-    let low = document.querySelector("#lowCheckbox");
-  
-    if (high.checked){
-      selectedPriorities.push("high");
-    }
-    if (medium.checked){
-      selectedPriorities.push("medium");
-    }
-    if (low.checked){
-      selectedPriorities.push("low");
-    }
-  
-    sessionStorage.setItem('selectedPriorites', `${selectedPriorities}`);
-    
-    loadPage();
-}
+let filter = () => {
+  let selectedPriorities = [];
+  let high = document.querySelector("#highCheckbox");
+  let medium = document.querySelector("#mediumCheckbox");
+  let low = document.querySelector("#lowCheckbox");
 
-let sort = async()=>{
+  if (high.checked) {
+    selectedPriorities.push("high");
+  }
+  if (medium.checked) {
+    selectedPriorities.push("medium");
+  }
+  if (low.checked) {
+    selectedPriorities.push("low");
+  }
+
+  sessionStorage.setItem("selectedPriorites", `${selectedPriorities}`);
+
+  loadPage();
+};
+
+let sort = async () => {
   //pushes or updates value to db after pressing button apply
-  let selectedRadio = document.querySelector('input[type="radio"][name="sort"]:checked');
-  
+  let selectedRadio = document.querySelector(
+    'input[type="radio"][name="sort"]:checked'
+  );
+
   if (selectedRadio) {
     sessionStorage.setItem("sorting", selectedRadio.value);
   }
@@ -310,17 +321,15 @@ let sort = async()=>{
   let allRadios = document.querySelectorAll('input[type="radio"][name="sort"]');
   let valueInStorage = sessionStorage.getItem("sorting");
 
-  allRadios.forEach(radio=>{
+  allRadios.forEach((radio) => {
     if (valueInStorage === radio.value) {
       radio.checked = true;
-    }else{
+    } else {
       radio.checked = false;
     }
   });
-}
+};
 
-document.querySelector("#filterButton").addEventListener("click",filter);
+document.querySelector("#filterButton").addEventListener("click", filter);
 document.querySelector("#applySorting").addEventListener("click", loadPage);
 loadPage();
-
-
