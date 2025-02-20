ebd8f17465934b9b80e772651ae3809410109372
JustinaAl <juste.paskeviciute@gmail.com>
2025-02-20 18:38:39 +0100

    Sidebar funktioner + linkat med habits och events.

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -1,8 +1,5 @@
-
-
 const checkLoginStatus = () =>{ 
-    userId = sessionStorage.getItem("userId");
-
+    let userId= sessionStorage.getItem("userId")
     if( !userId ){
         window.location.replace("index.html");
         return
@@ -19,3 +16,79 @@ logOutBtn.addEventListener('click', () => {
     sessionStorage.removeItem("userId");
     window.location.replace("index.html");
 })
+
+//Get habits
+let getHabits = async()=>{
+    let userId= sessionStorage.getItem("userId")
+    let data = await axios.get("http://localhost:5001/habits", { params: { userId }});
+    let allHabits = data.data;
+    allHabits.forEach((habit) => {
+        habit.reps = Number(habit.reps);
+      });
+
+    let sort = allHabits.sort((a, b) => b.reps - a.reps);
+    let lastThree = sort.slice(0,3);  
+    
+    return lastThree;
+}
+//Post habits
+let printOut = async()=>{
+    let lastThree = await getHabits();
+    let ul = document.querySelector("#navUlHabits");
+    let index = 0;
+    
+    lastThree.forEach((element) => {
+        let li = document.createElement("li");
+        li.textContent = element.title;
+        index+=1;
+        ul.append(li);
+    });
+}
+printOut();
+
+let todaysDate =()=> {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    return `${year}-${month}-${day}`;
+}
+
+let getEvents = async()=>{
+    let userId= sessionStorage.getItem("userId")
+    let today = todaysDate();
+    let todayToNr = today.replace(/-/g, '')
+
+    let data = await axios.get("http://localhost:5001/events", { params: { userId }});
+    let allEvents = data.data;
+
+    allEvents.forEach(element => {
+    let toNumber = element.startDate.replace(/[-\s:]/g, '').slice(0, -4);
+    element.startDate = Number(toNumber);    
+    });
+
+    let upcomingEvents = allEvents.filter(element => element.startDate >= Number(todayToNr));
+    let sort = upcomingEvents.sort((a, b) => a.startDate - b.startDate);
+    let lastThree = sort.slice(0,3);
+
+    
+    return lastThree;
+}
+
+let printOutEvents = async() => {
+    let lastThree = await getEvents();
+    let ul = document.querySelector("#navUlEvents");
+    let index = 0;
+    
+    lastThree.forEach((element) => {
+        let li = document.createElement("li");
+        li.textContent = element.title;
+        index+=1;
+        ul.append(li);
+    });
+}
+printOutEvents();
